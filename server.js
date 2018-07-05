33321e382a3eedacf9b9e11bfcd57cd2b077e46b
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-05 22:35:49 +0200

    Add project page.

--- server.js
+++ server.js
@@ -68,6 +68,12 @@ app.get('/bad',(request,response) => {
     });
 });
 
+app.get('/projects',(request,response) => {
+    response.render('projects.hbs',{
+        pageTitle: 'Projects'
+    });
+});
+
 app.listen(port,() => {
     console.log(`Server is up on port ${port}`);
 });
